3c190ec49a57d825609f3fc2cf2473c9354d5634
Sergey <shibut.sergey@gmail.com>
2023-04-13 22:57:55 +0400

    php

--- src/js/script.js
+++ src/js/script.js
@@ -52,5 +52,73 @@ $(document).ready(function () {
   };
   toggleSlide('.catalog-item__link');
   toggleSlide('.catalog-item__back');
+
+  //modal
   
+  $('[data-modal="consultation"]').on('click', function() {
+    $('.overlay, #consultation').fadeIn('slow');
+  });
+  $('.modal__close').on('click', function() {
+    $('.overlay, #consultation, #order, #thanks').fadeOut('slow');
+  });
+  
+  $('.button_mini').each(function(i) {
+    $(this).on('click', function() {
+      $('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
+      $('.overlay, #order').fadeIn('slow');
+    })
+  });
+
+  const overlay = document.querySelector('.overlay');
+ 
+    $('.overlay').on('click', function(e) {
+        if (e.target === overlay) {
+            $('.overlay, #consultation, #thanks, #order').fadeOut('slow');
+        }
+    });
+    
+    
+
+    function valideForms(form) {
+      $(form).validate( {
+        rules: {
+          name: 'required',
+          phone: 'required',
+          email: {
+            required: true,
+            email: true 
+          }
+        },
+        messages: {
+          name: "Введите имя",
+          phone: "Введите телефон",
+          email: {
+            required: "Нам нужен ваш емэйл",
+            email: "Введите правильный формат"
+          }
+        }
+      });
+    };
+    valideForms('#consultation-form');
+    valideForms('#consultation form');
+    valideForms('#order form');
+
+    $('input[name-phone]').mask("+7 (999) 999-99-99");
+
+    $('form').submit(function(e) {
+      e.preventDefault();
+      $.ajax({
+          type: "POST",
+          url: "mailer/smart.php",
+          data: $(this).serialize()
+      }).done(function() {
+          $(this).find("input").val("");
+          $('#consultation, #order').fadeOut();
+          $('.overlay, #thanks').fadeIn('slow');
+
+          $('form').trigger('reset');
+      });
+      return false;
+    });
+
 });
